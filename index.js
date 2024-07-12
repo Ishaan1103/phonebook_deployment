0ee7f96d2da1bc1a0aab9952c62d94f4e237a56f
Ishaan1103 <ishaanrana2635@gmail.com>
2024-07-12 22:38:37 +0530

    env added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import morgan from 'morgan';
 import express from 'express'
 import cors from 'cors'
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT||3001;
 app.use(cors())
 app.use(express.json());
 app.use(express.static('dist'))
